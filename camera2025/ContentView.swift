fdf9a640846fcc6e7f2d0ad971e9299139b1de75
Oleg Voytin <ovoytin@sberhealth.ru>
2025-02-02 14:05:20 +0100

    Initial Commit

---
+++ camera2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  camera2025
+//
+//  Created by Олег Войтин on 02.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ camera2025/camera2025App.swift
@@ -0,0 +1,17 @@
+//
+//  camera2025App.swift
+//  camera2025
+//
+//  Created by Олег Войтин on 02.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct camera2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
